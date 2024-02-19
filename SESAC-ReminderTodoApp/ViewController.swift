341bfd5c65d0d19497ea2ba365b4caef3e64af51
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2024-02-19 23:54:17 +0900

    Initial Commit

---
+++ SESAC-ReminderTodoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SESAC-ReminderTodoApp
+//
+//  Created by Seryun Chun on 2024/02/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
